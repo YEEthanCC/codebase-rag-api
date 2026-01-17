74686ed58a353bebb7d7786a8ac09ac07a911bec
Ethan Chen <ethan@DESKTOP-3CKM5U0.localdomain>
2026-01-16 16:58:08 -0800

    agent initialization update

--- services/repo_extension_service.py
+++ services/repo_extension_service.py
@@ -8,11 +8,11 @@ from ..codebase_rag.tools.codebase_query import create_query_tool
 from ..codebase_rag.services.llm import CypherGenerator, create_code_retrieval_tool
 from ..codebase_rag.tools.code_retrieval import CodeRetriever, create_code_retrieval_tool
 from ..codebase_rag.tools.file_extension_reader import FileExtensionReader, create_file_reader_tool
-from ..codebase_rag.tools.file_extension_writer import FileWriter, create_file_writer_tool
-from ..codebase_rag.tools.file_extension_editor import FileEditor, create_file_editor_tool
+from ..codebase_rag.tools.file_extension_writer import FileExtensionWriter, create_file_writer_tool
+from ..codebase_rag.tools.file_extension_editor import FileExtensionEditor, create_file_editor_tool
 from ..codebase_rag.tools.shell_extension_command import ShellCommander, create_shell_command_tool
-from ..codebase_rag.tools.directory_extension_lister import DirectoryLister, create_directory_lister_tool
-from ..codebase_rag.tools.document_extension_analyzer import DocumentAnalyzer, create_document_analyzer_tool
+from ..codebase_rag.tools.directory_extension_lister import DirectoryExtensionLister, create_directory_lister_tool
+from ..codebase_rag.tools.document_extension_analyzer import DocumentExtensionAnalyzer, create_document_analyzer_tool
 from ..codebase_rag.tools.semantic_search import create_semantic_search_tool, create_get_function_source_tool
 from ..codebase_rag.services.llm import CypherGenerator, create_rag_orchestrator
 from sockets.server import sio
@@ -49,15 +49,15 @@ def _initialize_services_and_agent(repo_path: str, ingestor: MemgraphIngestor, s
     cypher_generator = CypherGenerator()
     code_retriever = CodeRetriever(socket_id=socket_id, ingestor=ingestor)
     file_reader = FileExtensionReader(socket_id=socket_id)
-    file_writer = FileWriter(socket_id=socket_id)
-    file_editor = FileEditor(socket_id=socket_id)
+    file_writer = FileExtensionWriter(socket_id=socket_id)
+    file_editor = FileExtensionEditor(socket_id=socket_id)
     shell_commander = ShellCommander(
         project_root='.', 
         timeout=settings.SHELL_COMMAND_TIMEOUT, 
         socket_id=socket_id
     )
-    directory_lister = DirectoryLister(project_root=repo_path, socket_id=socket_id)
-    document_analyzer = DocumentAnalyzer(project_root=repo_path, socket_id=socket_id)
+    directory_lister = DirectoryExtensionLister(socket_id=socket_id)
+    document_analyzer = DocumentExtensionAnalyzer(socket_id=socket_id)
 
     query_tool = create_query_tool(ingestor, cypher_generator, console)
     code_tool = create_code_retrieval_tool(code_retriever)
